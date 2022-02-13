9168b30ff8de5931f6b47473fe5d3a426fc8a335
Howtopickaname <jqliao1993@gmail.com>
2022-02-13 21:33:34 +0800

    initial commit

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,10 @@
-
+import pandas as pd
 def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
+    df = pd.read_csv(reader, dtype={'Air Temperature': float})
+    df['Datetime'] = pd.to_datetime(df['Measurement Timestamp'], format='%m/%d/%Y %I:%M:%S %p')
+    df['Date'] = df['Datetime'].dt.strftime('%m/%d/%Y')
+    df.sort_values(['Station Name', 'Datetime'])
+    res = df.groupby(['Station Name', 'Date'])['Air Temperature'].agg(['min', 'max', 'first', 'last'])
+    res = res.rename(columns={'min': 'Min Temp', 'max': 'Max Temp', 'first': 'First Temp', 'last': 'Last Temp'})
+    csv = res.to_csv(date_format='%m/%d/%Y')
+    writer.write(csv)
